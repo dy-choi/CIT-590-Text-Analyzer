b7197be60aea0d730a4917410b08cbf9e5daaeab
Donna Choi <dchoi0920@gmail.com>
2018-04-16 12:58:48 -0400

    Updated challenge exercises

--- 0406_Text_Analyzer/src/rec0406/WordCounter.java
+++ 0406_Text_Analyzer/src/rec0406/WordCounter.java
@@ -2,7 +2,9 @@ package rec0406;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class WordCounter {
 
@@ -18,23 +20,49 @@ public class WordCounter {
 
 		// generate the word count from the lines read
 		generateWordCounts();
-		removeTrivialWords(); // this is a challenege exercise
-
-	}
-
-	/**
-	 * fills the map with words and counts
-	 */
-	protected void generateWordCounts() {
-
-		// TODO
-
+		removeTrivialWords();
 	}
 
 	/**
 	 * @return map of words to count totals
 	 */
-	protected Map<String, Integer> getWordCount() {
+	public void generateWordCounts() {
+
+		// iterate through all the lines
+		for (String line : lines) {
+
+			// split the line at empty spaces
+			String[] words = line.split(" ");
+
+			// iterate through the words
+			for (String word : words) {
+
+				// check if the word is empty
+				if (!word.isEmpty()) {
+					// CHALLENGE EXERCISE: removePunctuationsFromWord
+					word = removePunctuationsFromWord(word);
+
+					// check if the map already has this word
+					if (wordCount.containsKey(word)) {
+
+						// get the current count
+						int currentCount = wordCount.get(word);
+
+						// change the count to be 1 more
+						wordCount.put(word, currentCount + 1);
+
+					} else {
+
+						// add new word with count of 1
+						wordCount.put(word, 1);
+					}
+				}
+			}
+		}
+
+	}
+
+	public Map<String, Integer> getWordCount() {
 		return wordCount;
 	}
 
@@ -44,9 +72,21 @@ public class WordCounter {
 	 */
 	public void printWordsOccuringMoreThan(int threshold) {
 
+		// get map of words that occur more than threshold
 		Map<String, Integer> commonWordCount = getWordsOccuringMoreThan(threshold);
 
-		// TODO
+		// iterate through key set of map
+		for (String word : commonWordCount.keySet()) {
+
+			// format the output for each word: "count: word"
+			// "\t" is a tab character
+			String output = commonWordCount.get(word) + ":\t" + word;
+
+			// print the formatted output
+			System.out.println(output);
+
+		}
+
 	}
 
 	/**
@@ -56,9 +96,31 @@ public class WordCounter {
 	 */
 	public Map<String, Integer> getWordsOccuringMoreThan(int threshold) {
 
-		// TODO
+		// create a new map with all the values of the original, full word count
+		// map
+		Map<String, Integer> commonWordCount = new HashMap<String, Integer>(wordCount);
 
-		return null;
+		// get an iterator for the collection of entries in the map
+		Iterator<Entry<String, Integer>> iter = commonWordCount.entrySet().iterator();
+
+		// keeping going while there are still entries to analyze
+		while (iter.hasNext()) {
+
+			// get the Entry object from the iterator
+			Map.Entry<String, Integer> currentEntry = iter.next();
+
+			// get the count for this entry
+			Integer count = (Integer) currentEntry.getValue();
+
+			// if the count is below the threshold, remove it from the map
+			if (count < threshold) {
+				iter.remove();
+			}
+
+		}
+		// return the updated map
+		return commonWordCount;
+
 	}
 
 	/*
@@ -69,25 +131,35 @@ public class WordCounter {
 	 */
 
 	/**
-	 * Define trivial words to be words that are either single- or
-	 * double-characters long, such as "I", "a", "we", etc. Remove all such
-	 * words from this.wordCount's key set.
+	 * Define trivial words to be words that are of length three or less, such
+	 * as "I", "a", "we", "the", "and", etc. Remove all such words from
+	 * this.wordCount's key set.
 	 */
-	public void removeTrivialWords() {
-		// TODO
+	private void removeTrivialWords() {
+		Iterator<String> iter = this.wordCount.keySet().iterator();
+		// iterate through each element in the key set
+		while (iter.hasNext()) {
+			// remove if iter points to String with length less than three
+			if (iter.next().length() < 4) {
+				iter.remove();
+			}
+		}
 	}
 
 	/**
 	 * Remove all punctuations from a word. Punctuations here are defined as
-	 * anything that is not a letter or a digit. Do not use regex. If you do not
-	 * know what it is, don't worry about it.
+	 * anything that is not a letter or a digit.
 	 * 
 	 * @param word
 	 * @return
 	 */
 	private String removePunctuationsFromWord(String word) {
 		String ans = "";
-		// TODO: this is a challenge exercise.
+		// loop through all characters in a char[] of the word
+		for (Character c : word.toCharArray()) {
+			if (Character.isLetterOrDigit(c))
+				ans += c;
+		}
 		return ans;
 	}
 
